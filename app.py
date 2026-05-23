82fdfbe75943ab776a3022e0bd9fccdf648c1ade
nandan-xd <om21jagtap@gmail.com>
2026-05-24 01:52:49 +0530

    Fixed database issue

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///tasks.db')
-app.secret_key = 'your_secret_key'
+app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 app.permanent_session_lifetime = timedelta(weeks = 999)
